d6b3a27b94a5baeb92cd2f4a345cade475b8d3d2
Josh <joshcscurfield@gmail.com>
2024-10-28 15:59:21 +0000

    got a basic login and logout system going
    uses env to redirect based on session state

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,13 @@
-import { SignInButton } from "@clerk/nextjs";
+import { SignOutButton } from "@clerk/nextjs";
 
-export default function Home() {
+export default function Dashboard() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <div className="bg-sky-700 px-10 py-2 rounded-lg text-white text-6xl">
+        <h1>Dashboard</h1>
+      </div>
       <div className="bg-sky-700 px-3 py-1 rounded-lg text-white">
-        <SignInButton />
+        <SignOutButton />
       </div>
     </div>
   );
