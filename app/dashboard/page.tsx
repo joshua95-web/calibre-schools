dbf9820d7a94cb6677a4d902a416584a57498aa3
Josh <joshcscurfield@gmail.com>
2024-10-30 16:04:53 +0000

    wrapped protected elements  for teachers ONLY!!

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,4 @@
-import { SignOutButton } from "@clerk/nextjs";
+import { Protect, SignOutButton } from "@clerk/nextjs";
 import { auth, currentUser } from "@clerk/nextjs/server";
 
 export default async function Dashboard() {
@@ -8,15 +8,29 @@ export default async function Dashboard() {
   if (userId) {
     return (
       <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-        <div className="bg-sky-700 px-10 py-2 rounded-lg text-white text-6xl">
-          <h1>Dashboard</h1>
-        </div>
-        <div>
-          <p className="text-2xl">Welcome to your dashboard</p>
-        </div>
-        <div className="bg-sky-700 px-3 py-2 rounded-lg text-white text-6xl">
-          <SignOutButton />
-        </div>
+        <Protect
+          permission="org:sys_memberships:manage"
+          fallback={
+            <div className="flex items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+              <p className="bg-red-700 px-10 py-2 rounded-lg text-white text-6xl">
+                You do not have permission to view this content
+              </p>
+              <div className="bg-sky-700 px-3 py-2 rounded-lg text-white text-6xl">
+                <SignOutButton />
+              </div>
+            </div>
+          }
+        >
+          <div className="bg-sky-700 px-10 py-2 rounded-lg text-white text-6xl">
+            <h1>Dashboard</h1>
+          </div>
+          <div>
+            <p className="text-2xl">(a bunch of data only teachers can see)</p>
+          </div>
+          <div className="bg-sky-700 px-3 py-2 rounded-lg text-white text-6xl">
+            <SignOutButton />
+          </div>
+        </Protect>
         <pre>{JSON.stringify(user, null, 2)}</pre>;
       </div>
     );
