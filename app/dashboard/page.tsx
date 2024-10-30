b3a0d02ff27abbc6a4673f1ef550f35b94136e73
Josh <joshcscurfield@gmail.com>
2024-10-30 15:16:06 +0000

    more updates

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,14 +1,24 @@
 import { SignOutButton } from "@clerk/nextjs";
+import { auth, currentUser } from "@clerk/nextjs/server";
 
-export default function Dashboard() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <div className="bg-sky-700 px-10 py-2 rounded-lg text-white text-6xl">
-        <h1>Dashboard</h1>
+export default async function Dashboard() {
+  const { userId } = await auth();
+  const user = await currentUser();
+
+  if (userId) {
+    return (
+      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+        <div className="bg-sky-700 px-10 py-2 rounded-lg text-white text-6xl">
+          <h1>Dashboard</h1>
+        </div>
+        <div>
+          <p className="text-2xl">Welcome to your dashboard</p>
+        </div>
+        <div className="bg-sky-700 px-3 py-2 rounded-lg text-white text-6xl">
+          <SignOutButton />
+        </div>
+        <pre>{JSON.stringify(user, null, 2)}</pre>;
       </div>
-      <div className="bg-sky-700 px-3 py-1 rounded-lg text-white">
-        <SignOutButton />
-      </div>
-    </div>
-  );
+    );
+  }
 }
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,10 @@ import { SignInButton } from "@clerk/nextjs";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <div className="bg-sky-700 px-3 py-1 rounded-lg text-white">
+      <div>
+        <h1 className="text-6xl">Welcome to some random website</h1>
+      </div>
+      <div className="bg-sky-700 px-4 py-2 rounded-lg text-white text-6xl">
         <SignInButton />
       </div>
     </div>
