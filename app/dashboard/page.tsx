e8b32e4e0d3c42a966d0b847256ef9f253aee435
Joshua <joshcscurfield@gmail.com>
2024-11-01 11:22:51 +0000

    changed permission to role in the dashboard protect element

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -11,7 +11,7 @@ export default async function Dashboard() {
     return (
       <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
         <Protect
-          permission="org:sys_memberships:manage"
+          role="org:school_admin"
           fallback={
             <div className="flex items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
               <p className="bg-red-700 px-10 py-2 rounded-lg text-white text-6xl">
--- app/route/public.ts
+++ app/route/public.ts
@@ -1,1 +1,13 @@
-import { NextResponse }
+import { NextResponse } from "next/server";
+import { clerkClient } from "@clerk/nextjs/server";
+
+export async function POST() {
+  const { role, userId } = await body.json();
+
+  await clerkClient.users.updateUserMetadata(userId, {
+    publicMetadata: {
+      role,
+    },
+  });
+  return NextResponse.json({ success: true });
+}
